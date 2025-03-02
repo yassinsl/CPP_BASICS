5e2221e00c4be3b799e0726e1fd2cf4899108134
Yassine  Lahssini <ylahssin@e2r1p3.1337.ma>
2025-03-02 08:39:12 +0000

    update

---
+++ DAY00/class.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+class yassin
+{
+  public:
+    int a;
+     void display(void)
+     {
+       std::cout << "hello world" << std::endl;
+     }
+};
+int main(void)
+{
+  yassin ys;
+  ys.display();
+}
