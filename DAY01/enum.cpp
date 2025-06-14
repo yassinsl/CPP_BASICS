f6c5facc2686fd61b6b6b4c839912c942742c9eb
yassinsl <slawiyassin152@gmail.com>
2025-06-14 13:37:04 +0100

    understand samethinng

--- DAY01/enum.cpp
+++ DAY01/enum.cpp
@@ -6,7 +6,7 @@
 /*   By: ylahssin <ylahssin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/14 12:59:42 by ylahssin          #+#    #+#             */
-/*   Updated: 2025/06/14 13:15:58 by ylahssin         ###   ########.fr       */
+/*   Updated: 2025/06/14 13:36:04 by ylahssin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ enum var:char{AND, OR, SL}; //size enumaration var is 1;
 int main(void)
 {
 	int num = var::AND, num2 = OR;	
-	var yassin = var::SL;
+	var yassin = var::SL;// error (AND + 1);
 	cout << "this is num1 :"<< num << endl; 
 	cout << "this is num2 :"<< num2<< endl; 
 	cout << "this is enum variable : " << (int)yassin<< endl; 
