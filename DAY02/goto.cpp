8c88adf7f9f6380e1790f8613b78d007f29e757e
yassinsl <slawiyassin152@gmail.com>
2025-06-17 14:48:41 +0100

    goto

--- DAY02/goto.cpp
+++ DAY02/goto.cpp
@@ -1,35 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   more_vector.cpp                                    :+:      :+:    :+:   */
+/*   goto.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ylahssin <ylahssin@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/06/17 10:03:14 by ylahssin          #+#    #+#             */
-/*   Updated: 2025/06/17 12:15:09 by ylahssin         ###   ########.fr       */
+/*   Created: 2025/06/17 14:40:57 by ylahssin          #+#    #+#             */
+/*   Updated: 2025/06/17 14:48:06 by ylahssin         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
-#include <string>
-#include <vector>
 using namespace std;
-using str = string;
 
 int main(void)
 {
-	vector<str> names = {"Yassine", "Amin", "Issam"};
-	names.push_back("Slawi");
-	for(string name : names)
-		cout << "name is: " << name << endl;
-	cout 
-		<< "size the string vector is: "
-		<< names.size() 
-		<< endl;
-	int arr_1[] = {55, 39, 24};
-	for(int i : arr_1)
-		cout 
-			<< "content: "
-			<< i << endl;	
-	return 0;
+	int num = 3;
+
+	while(++num)
+		if(num == 9)
+			goto harba;
+	harba:
+		cout << "Bro has a jahed" << endl;
+	
 }
